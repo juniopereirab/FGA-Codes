690f87defa28e0fbe43501447e7869e27ab4e471
Dâmaso Júnio P. Brasileo <junio.pereirab@gmail.com>
2018-11-27 01:27:52 -0200

    Implementing some functions to linked list

--- algoritmos/linked list/list.h
+++ algoritmos/linked list/list.h
@@ -1,6 +1,10 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <iostream>
+
+using namespace std;
+
 template<typename T>
 
 class List {
@@ -76,6 +80,92 @@ public:
     tail == head ? (head = tail = nullptr) : (tail = prev, tail->next = nullptr);
     _size--;
   }
+
+  void append(List& L){
+    empty() ? head = L.head : tail->next = L.head;
+
+    tail = L.tail;
+    _size =+ L._size;
+
+    L.head = L.tail = nullptr;
+    L._size = 0;
+  }
+
+  void remove(const T& info){
+    if(!head){
+      throw -1;
+    }
+
+    if(info == front()){
+      pop_front();
+    }
+    else if(info == back()){
+      pop_back();
+    }
+    else{
+      auto p = head;
+      p = p->next;
+      if(p && p != tail){
+        auto temp = p;
+        while(p->info != info && p->next != tail){
+          temp = p;
+          p = p->next;
+          if(p->info == info){
+            temp->next = p->next;
+            delete p;
+            _size--;
+          } 
+        }
+      }
+      else{
+        throw "elemento nao encontrado!";
+      }
+    }
+  }
+
+  void remove_from(unsigned long pos){
+    if(!head){
+      throw "Lista Vazia";
+    }
+
+    if(pos == 1){
+      pop_front();
+    }
+    else if(pos == _size){
+      pop_back();
+    }
+    else{
+      auto p = head;
+      if(p && p != tail){
+        auto temp = p;
+        for(size_t i = 1; i < pos; ++i){
+          temp = p;
+          p = p->next;
+          if((i+1) == pos){
+            temp->next = p->next;
+            delete p;
+            _size--;
+          }
+        }
+      }
+      else{
+        throw "Posição não encontrada";
+      }
+    }
+  }
+
+  void print() 
+    {
+        auto p = head;
+
+        while (p)
+        {
+            auto next = p->next;
+            cout << p->info << " ";
+            p = next;
+        }
+        cout << endl;
+    }
 
 private:
   struct Node{
