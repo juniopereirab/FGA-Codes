a1847f92db8693065fd213762ea473c9a7cf5330
Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>
2019-09-30 09:17:57 -0300

    Adding Binary Tree basic implementation

---
+++ ED2/Implementações/Binary Tree/BinaryTree.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node Node;
+struct Node{
+  int info;
+  Node *left, *right;
+};
+
+Node *newNode(int data){
+  Node *node = (Node*)malloc(sizeof(Node));
+  node->info = data;
+  node->left = NULL;
+  node->right = NULL;
+  return(node);
+}
+
+int main(){
+  Node *root = newNode(1);
+  root->left = newNode(2);
+  root->right = newNode(3);
+  root->left->left = newNode(4);
+
+  printf("%d, %d, %d, %d", root->info, root->left->info, root->right->info, root->left->left->info);
+
+  return 0;
+}
