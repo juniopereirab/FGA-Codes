819c9a14aefc718f11f3254dbda86a5da3b43c1a
Dâmaso Júnio Pereira Brasileo <junio.pereirab@gmail.com>
2019-10-05 14:30:43 -0300

    Adding a example for creating a graphic about population growth

---
+++ Python/GraphProject/GraphClasses/PopulateGrowth.py
@@ -0,0 +1,23 @@
+# Crescimento da Populacao Brasileira 1980 - 2016
+# DataSUS
+import matplotlib.pyplot as plt
+
+dados = open("populacao-brasileira.csv").readlines()
+
+x = []
+y = []
+
+for i in range(len(dados)):
+    if i != 0:
+        linha = dados[i].split(";")
+        x.append(int(linha[0]))
+        y.append(int(linha[1]))
+
+
+plt.bar(x, y, color='#e4e4e4')
+plt.plot(x, y, linestyle='--', color='k')
+plt.scatter(x, y, color='')
+plt.title("Crescimento da Populacao Brasileira 1980 - 2016")
+plt.xlabel("Ano")
+plt.ylabel("Populacao x 100.000.000")
+plt.savefig("PopulationGrowth.png", dpi=300)
